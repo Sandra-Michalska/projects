aa9ed17e690855a6d0bd8bf61d107392cf6557aa
Sandra Michalska <sandra.kinga.michalska@gmail.com>
2023-11-28 11:07:28 +0100

    Add `Button` and `NoProjectSelected` components + render components on buttons click

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import NewProject from "./components/NewProject";
+import { useState } from "react";
 import Sidebar from "./components/Sidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
+import NewProject from "./components/NewProject";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleAddProjectButtonClick() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    });
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onAddProjectButtonClick={handleAddProjectButtonClick} />;
+  } else if(projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NewProject />
+      <Sidebar onAddProjectButtonClick={handleAddProjectButtonClick} />
+      {content}
     </main>
   );
 }
